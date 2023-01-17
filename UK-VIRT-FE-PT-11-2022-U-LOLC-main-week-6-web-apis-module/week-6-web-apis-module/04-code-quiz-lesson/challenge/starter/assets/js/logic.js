801bf1f0e4a28e073037cbe1fdaaf920f4a847bd
barrymoore1990 <hello@touringhighlights.com>
2023-01-17 13:32:36 +0000

    added feedback to answers

--- UK-VIRT-FE-PT-11-2022-U-LOLC-main-week-6-web-apis-module/week-6-web-apis-module/04-code-quiz-lesson/challenge/starter/assets/js/logic.js
+++ UK-VIRT-FE-PT-11-2022-U-LOLC-main-week-6-web-apis-module/week-6-web-apis-module/04-code-quiz-lesson/challenge/starter/assets/js/logic.js
@@ -3,21 +3,11 @@ let startQuiz = document.querySelector("#start");
 let startScreen = document.querySelector("#start-screen");
 let questionsScreen = document.querySelector("#questions");
 let choices = document.querySelector("#choices");
+let endScreen = document.querySelector("#end-screen");
+let feedback = document.querySelector("#feedback");
 
-// timer for quiz
-startQuiz.addEventListener("click", function(e) {
-    
-    let timer = 90;
-    let intervalId = setInterval(function(){
-    timer--;
-    timerText.textContent = timer;
-
-}, 1000);
-
-    startScreen.classList.add("hide");
-    questionsScreen.classList.remove("hide");
-    newQuestion();
-})
+let timer = 0;
+let currentQuestion = 0;
 
 // questions
 let questions = [
@@ -28,26 +18,72 @@ let questions = [
     {question: "5", answers:["ex1", "ex2", "ex3", "ex4"], correctAnswer: 2}
 ]
 
+// timer for quiz
+startQuiz.addEventListener("click", function(e) {
+    
+    timer = 90;
+    let intervalId = setInterval(function(){
+    timer--;
+    timerText.textContent = timer;
+    if (currentQuestion >= questions.length) {
+        clearInterval(intervalId);
+    }
 
-let currentQuestion = 0;
+}, 1000);
+
+    startScreen.classList.add("hide");
+    questionsScreen.classList.remove("hide");
+    newQuestion();
+    
+})
+
+// timer == 0 ||
+
 
 function newQuestion() {
-    choices.innerHTML = "";
-    document.querySelector("#question-title").textContent = questions[currentQuestion].question
+    if (currentQuestion >= questions.length) {
+        questionsScreen.classList.add("hide");
+        endScreen.classList.remove("hide");
+    } else {
+        choices.innerHTML = "";
+        document.querySelector("#question-title").textContent = questions[currentQuestion].question;
 
-    for (let i = 0; i < questions[currentQuestion].answers.length; i++) {
-        
-        let buttonCreate = document.createElement("button");
-        buttonCreate.textContent = questions[currentQuestion].answers[i];
-        buttonCreate.setAttribute("data-index", i);
-        choices.appendChild(buttonCreate);
+        for (let i = 0; i < questions[currentQuestion].answers.length; i++) {
+            
+            let buttonCreate = document.createElement("button");
+            buttonCreate.textContent = questions[currentQuestion].answers[i];
+            buttonCreate.setAttribute("data-index", i);
+            choices.appendChild(buttonCreate);
+        }
+
     }
+    
+    
+}
+
+let answer = "";
+
+function fback(answer) {
+    
+        feedback.classList.remove("hide");
+        feedback.textContent = answer;
+        
+        setInterval(function(){
+            feedback.classList.add("hide");
+        }, 1000);
+    
 }
 
 
 choices.addEventListener("click", function (event){
     if(event.target.matches("button")) {
-        console.log(event.target.getAttribute("data-index"));
+        answer = event.target.getAttribute("data-index");
+        if (answer != questions[currentQuestion].correctAnswer) {
+            timer -= 10;
+            fback("Wrong!");
+        } else {
+            fback("Correct!");
+        }
         currentQuestion++;
         newQuestion();
     }
@@ -61,8 +97,6 @@ for (let i = 0; i < questions.length; i++) {
 }
 
 
-
-// timer -= 20 would add on 20
 
 // On last question have some sort of game over function which stops timer
 // clearInterval(intervalId);
